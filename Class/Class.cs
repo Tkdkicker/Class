32e9b96c3e35070dae6c519a42b29c1ca807b308
gemma <gemma@DESKTOP-2Q2G917>
2022-11-16 09:51:21 +0000

    Add project files.

---
+++ Class/Class.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Class
+{
+    internal class Class
+    {
+        public string property = "Data";
+
+        public void print()
+        {
+            Console.WriteLine("Printing a message");
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Class myObject = new Class();
+                Console.WriteLine(myObject.property);
+                myObject.print();
+                Console.ReadKey();
+
+                Console.WriteLine("\nObject2\n");
+
+                Class myObject2 = new();
+
+                Console.WriteLine(myObject.property);
+                myObject.print();
+
+                myObject2.property = "Data, Data";
+                Console.WriteLine(myObject2.property);
+                myObject2.print();
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
